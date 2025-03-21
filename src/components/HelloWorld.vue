b449ab351699a611fe06bc388e06860f6ba055ca
AmeliReyesHernandez <22620050@tlaxiaco.tecnm.mx>
2025-03-20 18:52:26 -0600

    Subiendo carpeta

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+<!-- estructura html --> 
+ <h1>Dino</h1>
+ <button class="btn">Dinosaurio</button>
+
+ <img src="https://png.pngtree.com/png-vector/20241226/ourmid/pngtree-baby-dinosaur-cartoon-clipart-fun-and-cute-dino-design-png-image_14906503.png"/>
+
+<p>
+  Entre los dinosaurios más icónicos se encuentran el Tyrannosaurus rex, el Triceratops, el Velociraptor, 
+  el Diplodocus y el Braquiosaurio, cada uno con características únicas que continúan fascinando a científicos 
+  y al público en general
+</p>
+</template>
+
+<style scoped>
+/*estilos css*/
+h1{
+font-family: Verdana, Geneva, Tahoma, sans-serif;
+font-size: 45px;
+color: brown;
+}
+
+.btn{
+  border-radius: 15px;
+  background-color: #ffff;
+  color: black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.img{
+  width: 100px;
+  height: 20px;
+}
+
+</style>
